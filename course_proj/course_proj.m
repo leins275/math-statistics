4e1f1568428041148973a7a25562d4b35bda9ed2
leins275 <feitrich@yandex.ru>
2019-10-15 22:29:07 +0300

    Added pictures for course proj
    Graphics in png.

--- course_proj/course_proj.m
+++ course_proj/course_proj.m
@@ -1,4 +1,7 @@
+% Включение пакета signal для вычисления xcorr
+pkg load signal;
 
+% Считывание данных светимости
 file_name = '37000_SPD16x16.mat';
 data = load(file_name);
 
@@ -12,42 +15,31 @@ end
 dt = cell2mat(data.Data(1, 2)) * 1e-3;
 t_s = cell2mat(data.Data(2, 2))
 
+% Сопоставление индексов с временной шкалой
 t_e = t_s + (cnt_means - 1) * dt;
 t = t_s:dt:t_e;  
 
+% Выделение областей светимости выше и ниже экватора
 B_up = B(cnt_rows / 2 + 1:cnt_rows, :, :);
 B_low = B(1:cnt_rows / 2, :, :);
 
-
+% Нахождение светимостей областей выше и ниже экавтора
 sum_B_up = sum(sum(B_up));
 sum_B_up = sum_B_up(:)';
 sum_B_low = sum(sum(B_low));
 sum_B_low = sum_B_low(:)';
 
+% Введение рассматриваемого отрезка времени
 t_cons_s = 135;
 t_cons_e = 175;
 
+% Введение временного окна
 w_w = 1;
 t_cons_w = t_cons_s:w_w:t_cons_e;
 
-function R = r(x, y)
-  R_m = corrcoef(x, y);
-  R = R_m(1, 2);
-end
-
-function T = period(x)
-  n = length(x);
-  maxlag = round(n / 2);
-  minlag = round(n / 12.5);
-  r = xcorr(x, maxlag, 'unbiased');
-  r = r(maxlag + minlag + 1:2 * maxlag + 1);
-  [r_max, ind_max] = max(r);
-  T = ind_max + minlag - 1;
-end
-
 R = [];
-T_sum_B_up = [];
-T_sum_B_low = [];
+% T_sum_B_up = [];
+% T_sum_B_low = [];
 
 for t_w_s = t_cons_w(t_cons_w + w_w <= t_cons_e)
   t_w_e = t_w_s + w_w;
@@ -56,12 +48,17 @@ for t_w_s = t_cons_w(t_cons_w + w_w <= t_cons_e)
   sum_B_up_w = sum_B_up(ind_w);
   sum_B_low_w = sum_B_low(ind_w);
   
+  % Рассчёт коэффициента корреляции R между светимостями областей выше 
+  % и ниже экавтора
   R = [R r(sum_B_up_w, sum_B_low_w)];
   
-  T_sum_B_up = [T_sum_B_up period(sum_B_up_w) * dt];
-  T_sum_B_low = [T_sum_B_low period(sum_B_low_w) * dt];
+  % Нахождение для светимостей областей выше  и ниже экватора основного
+  % периода колебаний с помощью вычисления автокорреляции
+  % T_sum_B_up = [T_sum_B_up period(sum_B_up_w) * dt];
+  % T_sum_B_low = [T_sum_B_low period(sum_B_low_w) * dt];
 end
 
-f_sum_B_up = 1 ./ T_sum_B_up
-f_sum_B_low = 1 ./ T_sum_B_low
+% Нахождение для светимостей областей выше и ниже экавтора частоты колебаний
+% f_sum_B_up = 1 ./ T_sum_B_up;
+% f_sum_B_low = 1 ./ T_sum_B_low;
 
